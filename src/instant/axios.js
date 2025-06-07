dff605d147d0cced6df54b30ced7320f7b3bb057
Ankesh1A <ankeshbarahdiya05112002@gmail.com>
2025-06-07 14:38:16 +0530

    url update

--- src/instant/axios.js
+++ src/instant/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
- baseURL:'http://localhost:8082', 
+ baseURL:'https://tecnoexam-backend.onrender.com', 
   withCredentials: true,
 });
 
